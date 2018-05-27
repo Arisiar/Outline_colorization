9cca494784104874721b39f3fd10998da9929bd7
Arisiar <as2702131@gmail.com>
2018-05-27 23:49:58 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,6 @@ def load_data(image_path, n='default'):
         img = cv2.blur(img, (3,3))     
     elif n == 'gray':
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #img = cv2.fastNlMeansDenoising(img,3,3,7,21)
         img = np.resize(img, [256, 256, 1])
         
     img = np.array(img).astype(np.float)
@@ -25,21 +24,6 @@ def save_data(images):
     img = np.clip(img, 0, 255).astype(np.uint8)
     return img
 
-def merge(images, size):
-    h, w = images.shape[1], images.shape[2]
-    img = np.zeros((h * size[0], w * size[1], 3))
-    for idx, image in enumerate(images):
-        i = idx % size[1]
-        j = idx // size[1]
-        img[j*h:j*h+h, i*w:i*w+w, :] = image
 
-    return img
-
-def imsave(images, size, path):
-    return  cv2.imwrite(path, merge(images, size))
-
-
-def inverse_transform(images):
-    return (images+1.)/2.
 
 
