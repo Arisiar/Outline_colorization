c23675910c4264884b3d6a964acb1ec99e544ca6
Arisiar <as2702131@gmail.com>
2018-06-15 11:20:06 +0800

    Update ops.py

--- ops.py
+++ ops.py
@@ -184,28 +184,6 @@ def se_layer(net, out_dim, ratio, name):
         scale = net * excitation
         
         return scale
-def spectral_norm(w, iteration=1):
-   w_shape = w.shape.as_list()
-   w = tf.reshape(w, [-1, w_shape[-1]])
-
-   u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
-
-   u_hat = u
-   v_hat = None
-   for i in range(iteration):
-       v_ = tf.matmul(u_hat, tf.transpose(w))
-       v_hat = l2_norm(v_)
-
-       u_ = tf.matmul(v_hat, w)
-       u_hat = l2_norm(u_)
-
-   sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
-   w_norm = w / sigma
-
-   with tf.control_dependencies([u.assign(u_hat)]):
-       w_norm = tf.reshape(w_norm, w_shape)
-
-   return w_norm
     
 def concat(x, y, name):
     
